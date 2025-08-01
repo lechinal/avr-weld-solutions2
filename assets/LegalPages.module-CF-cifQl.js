845cd4fb805086eafb759f0d9e1f8278c8380875
Alin Lechintan <alin.lechintan@outlook.com>
2025-08-01 11:55:27 +0300

    Updates

---
+++ assets/LegalPages.module-CF-cifQl.js
@@ -0,0 +1,1 @@
+const e="_legalPage_hnehm_1",n="_container_hnehm_6",t="_gdprList_hnehm_31",s={legalPage:e,container:n,gdprList:t};export{s};
--- assets/LegalPages.module-DHk_JA_k.js
+++
@@ -1,1 +0,0 @@
-const e="_legalPage_1iwfj_1",a="_container_1iwfj_6",n={legalPage:e,container:a};export{n as s};
